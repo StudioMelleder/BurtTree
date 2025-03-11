9d4fe458a928f27279db9d3f83f52da969fbfb4a
devgev <leifminfagel@gmail.com>
2025-03-12 00:13:40 +0100

    Store nodes registered with AddCustomType() for clean up on _ExitTree()

--- addons/burttree/BurtTreePlugin.cs
+++ addons/burttree/BurtTreePlugin.cs
@@ -1,50 +1,37 @@
 #if TOOLS
 using Godot;
+using System.Collections.Generic;
 
 [Tool]
 public partial class BurtTreePlugin : EditorPlugin
 {
-    public override void _EnterTree()
-    {
-        // Tree Node
-        Texture2D treeIcon = GD.Load<Texture2D>("res://addons/burttree/icons/node-tree.png");
-        Script treeScript = GD.Load<Script>("res://addons/burttree/scripts/Tree.cs");
-        AddCustomType("BurtTree", "Node", treeScript, treeIcon);
+	List<string> registeredTypesList = new List<string>();
 
-        // Action Node
-        Texture2D actionIcon = GD.Load<Texture2D>("res://addons/burttree/icons/node-action.png");
-        Script actionScript = GD.Load<Script>("res://addons/burttree/scripts/ActionNode.cs");
-        AddCustomType("BurtTreeActionNode", "Node", actionScript, actionIcon);
+	public override void _EnterTree()
+	{
+		var registerCustomType = (string name, string script, string icon) =>
+		{
+			AddCustomType(
+				name, "Node",
+				GD.Load<Script>($"res://addons/burttree/scripts/{script}.cs"),
+				GD.Load<Texture2D>($"res://addons/burttree/icons/{icon}.png")
+			);
+			registeredTypesList.Add(name);
+		};
 
-        // Selector Node
-        Texture2D selectorIcon = GD.Load<Texture2D>("res://addons/burttree/icons/node-selector.png");
-        Script selectorScript = GD.Load<Script>("res://addons/burttree/scripts/Selector.cs");
-        AddCustomType("BurtTreeSelectorNode", "Node", selectorScript, selectorIcon);
+		registerCustomType("BurtTree", "Tree", "node-tree");
+		registerCustomType("BurtTreeActionNode", "ActionNode", "node-action");
+		registerCustomType("BurtTreeSelectorNode", "Selector", "node-selector");
+		registerCustomType("BurtTreeSequenceNode", "Sequence", "node-sequence");
+		registerCustomType("BurtTreeInverterNode", "Inverter", "node-inverter");
+		registerCustomType("BurtTreeRepeaterNode", "Repeater", "node-repeater");
+		registerCustomType("BurtTreeRepeaterUntilFailureNode", "RepeaterUntilFailure", "node-repeater");
+	}
 
-        // Sequence Node
-        Texture2D sequenceIcon = GD.Load<Texture2D>("res://addons/burttree/icons/node-sequence.png");
-        Script sequenceScript = GD.Load<Script>("res://addons/burttree/scripts/Sequence.cs");
-        AddCustomType("BurtTreeSequenceNode", "Node", sequenceScript, sequenceIcon);
-
-        // Inverter Node
-        Texture2D inverterIcon = GD.Load<Texture2D>("res://addons/burttree/icons/node-inverter.png");
-        Script inverterScript = GD.Load<Script>("res://addons/burttree/scripts/Inverter.cs");
-        AddCustomType("BurtTreeInverterNode", "Node", inverterScript, inverterIcon);
-
-        // Repeater Node
-        Texture2D repeaterIcon = GD.Load<Texture2D>("res://addons/burttree/icons/node-repeater.png");
-        Script repeaterScript = GD.Load<Script>("res://addons/burttree/scripts/Repeater.cs");
-        AddCustomType("BurtTreeRepeaterNode", "Node", repeaterScript, repeaterIcon);
-    }
-
-    public override void _ExitTree()
-    {
-        RemoveCustomType("BurtTree");
-        RemoveCustomType("Action");
-        RemoveCustomType("Selector");
-        RemoveCustomType("Sequence");
-        RemoveCustomType("Inverter");
-        RemoveCustomType("Repeater");
-    }
+	public override void _ExitTree()
+	{
+		foreach (var name in registeredTypesList)
+			RemoveCustomType(name);
+	}
 }
 #endif
