e404f280ce75da66accd61252a508597d009bafb
devgev <leifminfagel@gmail.com>
2025-03-12 00:43:18 +0100

    Reformat through retabbing to fix indentations

--- addons/burttree/BurtTreePlugin.cs
+++ addons/burttree/BurtTreePlugin.cs
@@ -5,33 +5,33 @@ using System.Collections.Generic;
 [Tool]
 public partial class BurtTreePlugin : EditorPlugin
 {
-	List<string> registeredTypesList = new List<string>();
+    List<string> registeredTypesList = new List<string>();
 
-	public override void _EnterTree()
-	{
-		var registerCustomType = (string name, string script, string icon) =>
-		{
-			AddCustomType(
-				name, "Node",
-				GD.Load<Script>($"res://addons/burttree/scripts/{script}.cs"),
-				GD.Load<Texture2D>($"res://addons/burttree/icons/{icon}.png")
-			);
-			registeredTypesList.Add(name);
-		};
+    public override void _EnterTree()
+    {
+        var registerCustomType = (string name, string script, string icon) =>
+        {
+            AddCustomType(
+                name, "Node",
+                GD.Load<Script>($"res://addons/burttree/scripts/{script}.cs"),
+                GD.Load<Texture2D>($"res://addons/burttree/icons/{icon}.png")
+            );
+            registeredTypesList.Add(name);
+        };
 
-		registerCustomType("BurtTree", "Tree", "node-tree");
-		registerCustomType("BurtTreeActionNode", "ActionNode", "node-action");
-		registerCustomType("BurtTreeSelectorNode", "Selector", "node-selector");
-		registerCustomType("BurtTreeSequenceNode", "Sequence", "node-sequence");
-		registerCustomType("BurtTreeInverterNode", "Inverter", "node-inverter");
-		registerCustomType("BurtTreeRepeaterNode", "Repeater", "node-repeater");
-		registerCustomType("BurtTreeRepeaterUntilFailureNode", "RepeaterUntilFailure", "node-repeater");
-	}
+        registerCustomType("BurtTree", "Tree", "node-tree");
+        registerCustomType("BurtTreeActionNode", "ActionNode", "node-action");
+        registerCustomType("BurtTreeSelectorNode", "Selector", "node-selector");
+        registerCustomType("BurtTreeSequenceNode", "Sequence", "node-sequence");
+        registerCustomType("BurtTreeInverterNode", "Inverter", "node-inverter");
+        registerCustomType("BurtTreeRepeaterNode", "Repeater", "node-repeater");
+        registerCustomType("BurtTreeRepeaterUntilFailureNode", "RepeaterUntilFailure", "node-repeater");
+    }
 
-	public override void _ExitTree()
-	{
-		foreach (var name in registeredTypesList)
-			RemoveCustomType(name);
-	}
+    public override void _ExitTree()
+    {
+        foreach (var name in registeredTypesList)
+            RemoveCustomType(name);
+    }
 }
 #endif
