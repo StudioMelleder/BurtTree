73e2a7ba7565682fb6bf9039fa25fb5b97aa2e0d
devgev <leifminfagel@gmail.com>
2025-03-11 23:50:23 +0100

    Implement repeater until failure node
    Like a repeater, RepeaterUntilFailure decorators will continue to reprocess their child. That is until the
    child finally returns a failure, at which point the repeater will return success to its parent.

---
+++ addons/burttree/scripts/RepeaterUntilFailure.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+namespace BurtTree
+{
+	[Icon("res://addons/burttree/icons/node-repeater.png")]
+	public partial class RepeaterUntilFailure : DecoratorNode
+	{
+		[Export] public int MaxRepeats { get; set; } = -1;
+
+		private int _repeatCount = 0;
+
+		public override NodeState Execute()
+		{
+			if (Child == null)
+			{
+				GD.PrintErr("RepeaterUntilFailure must have a child node.");
+				return NodeState.Failure;
+			}
+
+			NodeState result = Child.Execute();
+
+            if (result == NodeState.Failure)
+                return NodeState.Success;
+
+			if (result == NodeState.Success)
+			{
+				_repeatCount++;
+
+				if (MaxRepeats > 0 && _repeatCount >= MaxRepeats)
+				{
+					_repeatCount = 0;
+					return result;
+				}
+				else
+				{
+					return NodeState.Running;
+				}
+			}
+			return NodeState.Running;
+		}
+	}
+}
